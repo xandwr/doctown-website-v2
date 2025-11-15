4b8e49201a8a647b7b77af70349a7971e0e4d2e9
xandwr <xandwrp@gmail.com>
2025-11-14 17:20:43 -0700

    Setup svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import "../app.css";
+	import favicon from "$lib/assets/favicon.svg";
+	import Navbar from "../components/Navbar.svelte";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Navbar />
+<div>
+	{@render children()}
+</div>
